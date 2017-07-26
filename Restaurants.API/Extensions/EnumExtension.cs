92c1e38774e98c2fc1b2b03714aca8bcbd369da5
Ѓорче Цековски <cekovski.gjorche@students.finki.ukim.mk>
2017-07-26 02:28:10 +0200

    Extensions Added

---
+++ Restaurants.API/Extensions/EnumExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Restaurants.API.Extensions
+{
+    public static class EnumExtension
+    {
+        public static string GetEnumDescription<TEnum>(this TEnum item)
+            => item
+                .GetType()
+                .GetField(item.ToString())
+                .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                .Cast<DescriptionAttribute>()
+                .FirstOrDefault()?.Description ?? string.Empty;
+
+        public static void SeedEnumValues<TEntity, TEnum>(
+            this DbSet<TEntity> dbSet,
+            Func<TEnum, TEntity> converter) where TEntity : class
+            => Enum.GetValues(typeof(TEnum))
+                .Cast<object>()
+                .Select(value => converter((TEnum)value))
+                .ToList()
+                .ForEach(instance => dbSet.AddOrUpdate(instance));
+    }
+}
